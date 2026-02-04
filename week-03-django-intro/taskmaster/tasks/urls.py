487befaa01517cc6da3868b8baaeed3c31bb5aeb
Jubril Tayo <jubriltayo@gmail.com>
2026-02-04 17:52:03 +0100

    django intro - week 3

---
+++ week-03-django-intro/taskmaster/tasks/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = "tasks"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("tasks/", views.task_list, name="task_list"),
+    path("tasks/<int:task_id>", views.task_detail, name="task_detail"),
+    path("about/", views.about, name="about"),
+    path("stats/", views.stats, name="stats"),
+]
