91cde993cac41dd7164f62de942bf1c299a5f1a4
Jubril Tayo <jubriltayo@gmail.com>
2026-02-08 21:25:50 +0100

    Models Basics - Django ORM

--- week-03-django-intro/taskmaster/tasks/urls.py
+++ week-03-django-intro/taskmaster/tasks/urls.py
@@ -6,7 +6,7 @@ app_name = "tasks"
 urlpatterns = [
     path("", views.index, name="index"),
     path("tasks/", views.task_list, name="task_list"),
-    path("tasks/<int:task_id>", views.task_detail, name="task_detail"),
+    path("tasks/<int:task_id>/", views.task_detail, name="task_detail"),
     path("about/", views.about, name="about"),
     path("stats/", views.stats, name="stats"),
 ]
--- week-03-django-intro/taskmaster/tasks/views.py
+++ week-03-django-intro/taskmaster/tasks/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpRequest, HttpResponse
+from django.db.models import Count, Q, Avg
+from .models import Task, Status
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -6,19 +8,31 @@ def index(request: HttpRequest) -> HttpResponse:
 
 
 def task_list(request: HttpRequest) -> HttpResponse:
-    tasks = [
-        {"id": 1, "title": "Learn Django", "status": "in_progress"},
-        {"id": 2, "title": "Build an app", "status": "pending"},
-    ]
+    tasks = Task.objects.select_related("category").all()
+
     html = "<h1>Tasks</h1><ul>"
     for task in tasks:
-        html += f"<li><a href='/tasks/{task['id']}/'>{task['title']}</a> - {task['status']}</li>"
+        category_name = task.category.name if task.category else "No Category"
+        html += f"<li><a href='/tasks/{task.id}/'>{task.title}</a> - {task.status} - {category_name}</li>"
     html += "</ul>"
+
     return HttpResponse(html)
 
 
 def task_detail(request: HttpRequest, task_id: int) -> HttpResponse:
-    return HttpResponse(f"<h1>Task #{task_id}</h1>")
+    task = Task.objects.select_related("category").get(id=task_id)
+    category_name = task.category.name if task.category else "No Category"
+
+    html = f""""
+    <h1>Task #{task.id}</h1>
+    <p>Title: {task.title}</p>
+    <p>Status: {task.status}</p>
+    <p>Priority: {task.priority_display}</p>
+    <p>Category: {category_name}</p>
+    <p>Description: {task.description}</p>
+    """
+
+    return HttpResponse(html)
 
 
 def about(request: HttpRequest) -> HttpResponse:
@@ -26,4 +40,17 @@ def about(request: HttpRequest) -> HttpResponse:
 
 
 def stats(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Task statistics</h1>")
+    summary = Task.objects.aggregate(
+        total_tasks=Count("id"),
+        completed_tasks=Count("id", filter=Q(status=Status.COMPLETED)),
+        average_priority=Avg("priority"),
+    )
+
+    html = f"""
+    <h1>Task Statistics</h1>
+    <p>Total tasks: {summary['total_tasks']}</p>
+    <p>Completed tasks: {summary['completed_tasks']}</p>
+    <p>Average priority: {summary['average_priority']}</p>
+    """
+
+    return HttpResponse(html)
